7f001d79db26ce75b701ec629abf9c9d4141163f
Ignacio Cassi <nacho.cassi97@gmail.com>
2021-03-19 22:33:58 -0300

    Agregados métodos y atributos estáticos

--- DDRJavaPOO-proyecto/DDRJavaPOO/src/Empleado.java
+++ DDRJavaPOO-proyecto/DDRJavaPOO/src/Empleado.java
@@ -1,4 +1,5 @@
 public class Empleado {
+
     //Constantes
     private final static double SALARIO_DEFAULT=40000;
 
@@ -6,7 +7,12 @@ public class Empleado {
     private String nombre;
     private String apellido;
     private int edad;
+    private static double salarioBase=1000.0;
     private double salario;
+    /*Una variable o método estático es aquel es igual en todas las clases, es decir,
+     que aunque creemos distintos objetos en todos sera igual. Se define escribiendo 
+     static en la definición del método o del atributo.*/
+    
 
     //Métodos:
     //Constructor vacío:
@@ -30,7 +36,6 @@ public class Empleado {
         this(nombre,apellido,edad,SALARIO_DEFAULT);
     }
     
-    //Método constructor:
     //Constructor con los 4 param:
     public Empleado(String nombre, String apellido, int edad, double salario) {
         this.nombre=nombre;
@@ -83,6 +88,22 @@ public class Empleado {
             "}";
     }
 
+    //Metodo estático:
+    
+    /*Debemos tener en en cuenta que si un método es estático,
+    los atributos que estén dentro del método deben ser estáticos también,
+     si hay alguno que no lo es, saltara un error.*/
+     
+    public static double getSalarioBase() {
+        return Empleado.salarioBase;
+    }
+
+    //Metodo estático:
+    public static void setSalarioBase(double salarioBase) {
+        //Modifica el atributo estático, no una instancia.
+        Empleado.salarioBase=salarioBase;
+    }
+
     //Métodos extras:
     public boolean plus(double sueldoPlus) {
         //Suma un sueldoPlus al salario si el empleado tiene más de 40 años.
@@ -96,4 +117,14 @@ public class Empleado {
            //Devuelve true si aplico el aumento, false si no.
            return aumento;
     }
+
+    //Devuelve verdadero si el Empleado tiene nombre, falso si es cadena vacia ""
+    boolean compruebaNombre() {
+        if (nombre=="") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
